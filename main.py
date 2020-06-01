a8e9d3cb66a33a56f2c1bea215c24a911e96098b
ivakorin <werewolfe@yandex.ru>
2020-06-01 20:37:24 +0500

    Removed temp_sensor func from  led  func

--- main.py
+++ main.py
@@ -21,6 +21,11 @@
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
 #
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
 
 
 import gc
@@ -400,7 +405,6 @@ def led(rgb):
     np = neopixel.NeoPixel(machine.Pin(led_pin), led_qty)
     np.fill(rgb)
     np.write()
-    temp_sensor()
     print(str(rgb))
 
 
