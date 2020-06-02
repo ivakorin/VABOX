ab6f98e7eeee800dab777a9ba5efafeaa3461bd9
ivakorin <werewolfe@yandex.ru>
2020-06-02 22:28:38 +0500

    deleted some files

---
+++ tinyweb/__init__.py
@@ -0,0 +1,14 @@
+"""
+Tiny Web - pretty simple and powerful web server for tiny platforms like ESP8266 / ESP32
+MIT license
+(C) Konstantin Belyalov 2017-2018
+"""
+#  __init__.py from vabox
+#  Copyright (C) 2020  Ignat Vakorin
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+
+from .server import webserver
